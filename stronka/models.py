d31ab0f103881b9246c90bf8a9e9277510c52adc
Karol Nadolny <karolek9.10@o2.pl>
2019-04-08 17:27:41 +0200

    Modele- początek

--- stronka/models.py
+++ stronka/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+
+class Team(models.Model):
+    ID = models.IntegerField(primary_key =  True)
+    Name = models.CharField(max_length = 30)
+    Register_Date = models.DateField(default = datetime.now(),blank = False)
+    City = models.CharField(max_length = 50)
+
+    Company = models.CharField(max_length = 100)

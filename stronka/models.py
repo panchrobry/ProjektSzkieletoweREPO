b7430c9f75c780c14ab0ab5acd73bc1b8bae3100
Karol Nadolny <karolek9.10@o2.pl>
2019-04-09 11:08:49 +0200

    Działania na modelach

--- stronka/models.py
+++ stronka/models.py
@@ -5,9 +5,24 @@ from datetime import datetime
 # Create your models here.
 
 class Team(models.Model):
-    ID = models.IntegerField(primary_key =  True)
+    ID = models.CharField(primary_key = True,max_length = 3)
     Name = models.CharField(max_length = 30)
     Register_Date = models.DateField(default = datetime.now(),blank = False)
     City = models.CharField(max_length = 50)
-
     Company = models.CharField(max_length = 100)
+
+
+class User(models.Model):
+
+    Username = models.CharField(max_length = 30)
+    Forename = models.CharField(max_length = 50)
+    Surname = models.CharField(max_length = 50)
+    TeamID = models.ForeignKey(Team,on_delete=models.CASCADE)
+    Email = models.EmailField(max_length = 50)
+    TShirtSize = (
+        ('S','SMALL'),
+        ('M','MEDIUM'),
+        ('L','LARGE'),
+        ('XL','EXTRALARGE'),
+        ('XXL','EXTRAEXTRALARGE'),
+    )

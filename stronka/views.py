35a8045cdf5c2e713001a8e571c3d6b2ae4d1a13
Karol Nadolny <karolek9.10@o2.pl>
2019-04-26 21:52:53 +0200

    Działająca podstawowa rejestracja :D

--- stronka/views.py
+++ stronka/views.py
@@ -1,20 +1,20 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, HttpResponse, redirect
 from .models import User
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
+
+
 def home(request):
+
     return render(request, 'accounts/home.html')
-
-
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/account/home')
+            return redirect('account/home/')
     else:
-        form = UserCreationForm()
-
+        form = UserCreationForm
         args = {'form':form}
         return render(request,'accounts/reg_form.html',args)
